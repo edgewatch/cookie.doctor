d48f0e27bb7f1bef040f3c4f979d7284e7b9aea4
Gurjant Singh <g.singh@occentus.net>
2024-09-30 11:35:21 +0200

    Cookie scanner created Requirements added

---
+++ cookie_scanner.py
@@ -0,0 +1,78 @@
+import datetime
+import json
+import os
+import sys
+from concurrent.futures import ThreadPoolExecutor
+import requests
+import uuid
+
+# API_COOKIE_DOCTOR='https://api.cookie.doctor/api/v1/process_host/'
+API_COOKIE_DOCTOR='http://0.0.0.0:8000/api/v1/process_host/'
+
+def read_hostnames(input_arg):
+    """Read hostnames from a single hostname, list of hostnames, or a .txt file."""
+    if isinstance(input_arg, list):
+        return input_arg
+    elif input_arg.endswith('.txt'):
+        with open(input_arg, 'r') as f:
+            return [line.strip() for line in f.readlines()]
+    else:
+        return [input_arg]
+
+def write_output(response: str, hostname: str, date_time:str ):
+    """Write the response to a output/hostname/json file."""
+    file_ext = 'json'
+    date_time = date_time.replace(" ", "_")
+    hostname = hostname.replace("/", "_")
+    try:
+        response = json.loads(response)
+    except json.JSONDecodeError:
+        file_ext = 'txt'
+    
+    filename = f"output/{hostname}/{hostname}_{date_time}.{file_ext}"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    
+    if file_ext == 'json':
+        with open(filename, 'w') as f:
+            json.dump(response, f, indent=4)
+    else:
+        with open(filename, 'w') as f:
+            f.write(response)
+    print("Response saved to", filename)
+
+def send_request(hostname):
+    """Send a request to api.cookie.doctor with the hostname."""
+    try:
+        date_time = str(datetime.datetime.now())
+        hostname = f'https://{hostname}' if not hostname.startswith('http') else hostname
+        response = requests.post(API_COOKIE_DOCTOR, json={
+            'url': hostname,
+            'is_public': True,
+            'start_at': date_time,
+            'channel_name': str(uuid.uuid4()),
+        })
+        if (response.status_code == 200) or (response.status_code == 201):
+            write_output(response.text, hostname, date_time)
+        else:
+            print(f"Response for {hostname}, Status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {hostname}, Message: {str(e)}")
+    except Exception as e:
+        print(f"Error: {hostname}, Message: {str(e)}")
+
+def process_hostnames(hostnames):
+    """Process a list of hostnames using 4 workers with ThreadPoolExecutor."""
+    with ThreadPoolExecutor(max_workers=4, thread_name_prefix=f"process_host_workers") as executor:
+        executor.map(send_request, hostnames)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: script.py <hostname|hostnames.txt|['hostname1', 'hostname2', ...]>")
+    else:
+        input_arg = sys.argv[1]
+        if input_arg.startswith("[") and input_arg.endswith("]"):
+            # Convert string list argument to actual list
+            hostnames = eval(input_arg)
+        else:
+            hostnames = read_hostnames(input_arg)
+        process_hostnames(hostnames)
